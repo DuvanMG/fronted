795f3e83e687009c4d5825b33863504e0e153acf
DuvanMG <Duvanoved@hotmail.com>
2024-09-28 14:44:45 -0500

    render

--- pages/login/index.js
+++ pages/login/index.js
@@ -27,7 +27,8 @@ const Login = () => {
         setError('');
 
         try {
-            const response = await fetch('http://localhost:8000/api/login', {
+            const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
+            const response = await fetch(`${API_URL}/api/login`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
